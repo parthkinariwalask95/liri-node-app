3c4bf33e50ed4f7ab9837434bac68df541a8334b
parthkinariwalask95 <parthkinariwalask95@gmail.com>
2018-07-24 15:46:42 -0400

    final1

--- key.js
+++ key.js
@@ -1,16 +1,13 @@
-var Twitter = require('twitter.js');
- 
-var client = new Twitter({
-  consumer_key: 'NAcgxnp5NL4lgxOUqKEORqD60',
-  consumer_secret: 'SSnkbnoJFb1B5mDsSdr9YFgvdpBTMCQL9z4z3uzSjdrxUYZWJW',
-  access_token_key: '1020383961007239168-LxpWbFG7RYI1yeITfsp4YunjgY3ncl',
-  access_token_secret: 'buJ82aqu7cuHyNJ3b9H6XpxSV7ovv1zHYXwU4WXjy32wn'
-});
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) 
-  {
-    console.log(response);
-  }
-});
+// console.log("keys");
+exports.twitter = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  };
+  
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
